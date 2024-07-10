4e15d9c859980229fde872e49456d342d4a90cde
swaraj bachu <rajswaraj.r@gmail.com>
2024-07-10 19:03:48 +0530

    fix builds

--- src/app/docs/[...slug]/page.tsx
+++ src/app/docs/[...slug]/page.tsx
@@ -5,7 +5,7 @@ import { notFound } from "next/navigation";
 import "@/styles/mdx.css";
 import { Metadata } from "next";
 import { siteConfig } from "@/lib/site";
-import { Tag } from "@/components/tags";
+// import { Tag } from "@/components/tags";
 interface PostPageProps {
   params: {
     slug: string[];
@@ -75,9 +75,9 @@ export default async function PostPage({ params }: PostPageProps) {
     <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
       <h1 className="mb-2">{post.title}</h1>
       <div className="flex gap-2 mb-2">
-        {post.tags?.map((tag) => (
+        {/* {post.tags?.map((tag) => (
           <Tag tag={tag} key={tag} />
-        ))}
+        ))} */}
       </div>
       {post.description ? (
         <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
