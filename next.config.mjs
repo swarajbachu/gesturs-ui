6234b1f190b0b52040533b49bfd74a3db9446461
swaraj bachu <rajswaraj.r@gmail.com>
2024-07-10 12:19:27 +0530

    basic docs setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,51 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+import rehypeShiki from "rehype-shiki";
+import { build } from "velite";
 
-export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https",
+      },
+    ],
+  },
+  webpack: config => {
+    config.plugins.push(new VeliteWebpackPlugin())
+    return config
+  }
+  // Optionally, add any other Next.js config below
+};
+
+class VeliteWebpackPlugin {
+  static started = false
+  apply(/** @type {import('webpack').Compiler} */ compiler) {
+    // executed three times in nextjs
+    // twice for the server (nodejs / edge runtime) and once for the client
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      const { build } = await import('velite')
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
+
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypeShiki],
+  },
+});
+
+export default withMDX(nextConfig);
